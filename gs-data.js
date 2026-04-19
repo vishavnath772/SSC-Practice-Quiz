1b5f6fa15f19870391fa9a0100164797d259fc46
Vishavnath <vishavnathkumar772@gmail.com>
2026-04-19 21:37:21 +0530

    Update gs-data.js

--- gs-data.js
+++ gs-data.js
@@ -1,8 +1,7 @@
 const gsQuestions = [
-["Which of the following folk dances is performed by the fisherfolk of the Mithila region in Bihar?","Karma","Nrityakali","Kamala","Jiya","c","Kamala. Folk dances of Bihar : Karma, Kajri, Jhumar, Jharni, Jat Jatin, Jhumeri, Sohar Khelwana, Natua, Nachini, Jhijiya, Dhamar jogira, Krishi Nritya.","Art & Culture","Art & Culture"],
-["Chad Laho is a dance-festival, primarily celebrated by which of the following tribes of Meghalaya?","Khasi","Garo","Jaintia","Hojang","c","Jaintia. Chad Laho dance festival is celebrated as a thanksgiving festival to God after the harvesting season. Other folk dances of Meghalaya and their Tribes: Ka Shad Suk Mynsiem (Khasis tribe), Beh-dien-khlam (Jaintias), Wangala (Garos), Chad Sukra (Jaintia).","Art & Culture","Art & Culture"],
-["Which of the following dances of Goa is performed to celebrate the victory of the Maratha warriors over the Portuguese ?","Ghodemodni","Zemmado","Morulem","Goff","a","Ghodemodni (Horse dance) celebrated in Thane, Sattari, and nearby villages.","Art & Culture","Art & Culture"],
-["Which of the following war dances is performed in Ngada festival and is performed by the tribes of Nagaland?","Jawara","Dalkhai","Rengma","Garadi","c","Rengma. Other popular dances of Nagaland include Modse, Aaluyattu.","Art & Culture","Art & Culture"],
-["In which of the following classical dance forms are the actors’ faces painted to symbolise the type of character they are playing?","Bharatanatyam","Odissi","Kathakali","Kathak","c","Kathakali makeup: Pacha (noble gods), Katti (anti-heroes), Thadi (bearded roles).","Art & Culture","Art & Culture"]
+["Which of the following folk dances is performed by the fisherfolk of the Mithila region in Bihar?","Karma","Nrityakali","Kamala","Jiya","c","Kamala. Folk dances of Bihar : Karma, Kajri, Jhumar, etc.","Art & Culture","Art & Culture"],
+["Chad Laho is a dance-festival, primarily celebrated by which of the following tribes of Meghalaya?","Khasi","Garo","Jaintia","Hojang","c","Jaintia. Chad Laho is a thanksgiving festival.","Art & Culture","Art & Culture"],
+["Which of the following dances of Goa is performed to celebrate the victory over the Portuguese ?","Ghodemodni","Zemmado","Morulem","Goff","a","Ghodemodni (Horse dance) celebrated in Goa.","Art & Culture","Art & Culture"],
+["Which of the following war dances is performed in Ngada festival in Nagaland?","Jawara","Dalkhai","Rengma","Garadi","c","Rengma is a war dance of Nagaland.","Art & Culture","Art & Culture"],
+["In which classical dance form are the actors’ faces painted?","Bharatanatyam","Odissi","Kathakali","Kathak","c","Kathakali makeup symbolises characters.","Art & Culture","Art & Culture"]
 ];
-
